32d4763af61491d95ad0d932885de39c3c42a8a6
Richie O'Gorman <richieogorman@MacBook-Pro.local>
2026-04-05 00:19:56 +1100

    fix: Add GHOST_REGISTRY export to chains.ts for DeployTBA

--- app/utils/chains.ts
+++ app/utils/chains.ts
@@ -14,6 +14,8 @@ export const gnosis = defineChain({
   },
 });
 
+export const GHOST_REGISTRY = (process.env.NEXT_PUBLIC_GHOST_REGISTRY || '0x73F2d7f43B3aa98D434F53e921d3A41aa570bE13') as `0x${string}`;
+
 export const GNO_REGISTRARS = {
   nftmail: '0x831ddd71e7c33e16b674099129E6E379DA407fAF' as `0x${string}`,
 } as const;
