b7efa176207fe87ddfe2a298dc309506aea26ba8
Richie O'Gorman <richieogorman@MacBook-Pro.local>
2026-04-05 00:28:58 +1100

    fix: Add BRAIN_MODULE export to chains.ts

--- app/utils/chains.ts
+++ app/utils/chains.ts
@@ -16,6 +16,8 @@ export const gnosis = defineChain({
 
 export const GHOST_REGISTRY = (process.env.NEXT_PUBLIC_GHOST_REGISTRY || '0x73F2d7f43B3aa98D434F53e921d3A41aa570bE13') as `0x${string}`;
 
+export const BRAIN_MODULE = '0x291e8405096413407c3Ddd8850Fb101b446f5200' as `0x${string}`;
+
 export const GNO_REGISTRARS = {
   nftmail: '0x831ddd71e7c33e16b674099129E6E379DA407fAF' as `0x${string}`,
 } as const;
