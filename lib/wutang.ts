a66d789ebcd4437206ac4feeb2b458544aef8849
Richie O'Gorman <richieogorman@MacBook-Pro.local>
2026-04-07 19:52:44 +1000

    feat: use user-specified Roman numerals
    Limited to the specific Roman numerals provided:
    II, III, IV, VI, XI, VII, XII, XV, XX, XIII, XIV, XXX, XLV, LXV, LXX, XLI, XXI, IX, XIX, XXV, XCV, XCI, XC, LXI, LI, LII, LIV, LVI, LIX, XVI

--- lib/wutang.ts
+++ lib/wutang.ts
@@ -20,16 +20,8 @@ const WU_SUFFIXES = [
  * @returns Roman numeral string
  */
 function generateRomanNumeral(): string {
-  const romanNumerals = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 
-                        'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX',
-                        'XXI', 'XXII', 'XXIII', 'XXIV', 'XXV', 'XXVI', 'XXVII', 'XXVIII', 'XXIX', 'XXX',
-                        'XXXI', 'XXXII', 'XXXIII', 'XXXIV', 'XXXV', 'XXXVI', 'XXXVII', 'XXXVIII', 'XXXIX', 'XL',
-                        'XLI', 'XLII', 'XLIII', 'XLIV', 'XLV', 'XLVI', 'XLVII', 'XLVIII', 'XLIX', 'L',
-                        'LI', 'LII', 'LIII', 'LIV', 'LV', 'LVI', 'LVII', 'LVIII', 'LIX', 'LX',
-                        'LXI', 'LXII', 'LXIII', 'LXIV', 'LXV', 'LXVI', 'LXVII', 'LXVIII', 'LXIX', 'LXX',
-                        'LXXI', 'LXXII', 'LXXIII', 'LXXIV', 'LXXV', 'LXXVI', 'LXXVII', 'LXXVIII', 'LXXIX', 'LXXX',
-                        'LXXXI', 'LXXXII', 'LXXXIII', 'LXXXIV', 'LXXXV', 'LXXXVI', 'LXXXVII', 'LXXXVIII', 'LXXXIX', 'XC',
-                        'XCI', 'XCII', 'XCIII', 'XCIV', 'XCV', 'XCVI', 'XCVII', 'XCVIII', 'XCIX', 'C'];
+  // User-specified Roman numerals
+  const romanNumerals = ['II', 'III', 'IV', 'VI', 'XI', 'VII', 'XI', 'XII', 'XV', 'XX', 'XIII', 'XIV', 'XXX', 'XLV', 'LXV', 'LXX', 'XLI', 'XXI', 'IX', 'XIX', 'XXV', 'XCV', 'XCI', 'XC', 'LXI', 'LI', 'LII', 'LIV', 'LVI', 'LIX', 'XVI'];
   return romanNumerals[Math.floor(Math.random() * romanNumerals.length)];
 }
 
